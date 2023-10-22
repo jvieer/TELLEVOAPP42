8d3bc4e3c9caf5d542655a73015cdf90d969b38f
jvieer <javi.valenzuelav@duocuc.cl>
2023-10-22 20:16:53 -0300

    ya nose, muchos cambios

--- src/app/pages/viajes/createviaje/createviaje.page.ts
+++ src/app/pages/viajes/createviaje/createviaje.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ViajesTomadosService } from 'src/app/viajestomados.service';
-
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-createviaje',
@@ -27,12 +27,50 @@ export class CreateViajePage implements OnInit {
     if (this.viajeSeleccionado !== null) {
       // Implementa la lógica para "tomar" el viaje seleccionado
       console.log(`Viaje seleccionado: ${this.viajeSeleccionado}`);
-      // Si deseas desmarcarlo después de tomarlo, puedes hacerlo aquí
+    
       this.viajeSeleccionado = null;
     } else {
       // Muestra un mensaje de error o notificación si no se seleccionó ningún viaje.
       console.log('Selecciona un viaje antes de tomarlo');
     }
+  }
+
+  mensajec() {
+    Swal.fire({
+      title: '¿Estás seguro de tomar este viaje?',
+      icon: 'question',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Sí, tomar',
+      heightAuto: false,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.tomarViaje();
+        window.location.href = 'viajes-c';
+        this.mensaje();
+
+      }
+    });
+  }
+
+  mensaje(){
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 1500,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+      }
+    })
+    
+    Toast.fire({
+      icon: 'success',
+      title: 'Viaje tomado!'
+    })
   }
 }
 
