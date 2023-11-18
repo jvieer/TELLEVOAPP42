4458d6ae253f4928556576378fca2362858cde3b
jvieer <javi.valenzuelav@duocuc.cl>
2023-11-18 13:20:57 -0300

    a medias los roles, falta cambiar el register

--- src/app/pages/register/register.page.ts
+++ src/app/pages/register/register.page.ts
@@ -3,7 +3,6 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { TranslateService } from '@ngx-translate/core';
 import { AuthService } from 'src/app/services/firebase/auth.service';
-import { UsuariosService } from 'src/app/services/usuarios.service';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -15,22 +14,23 @@ export class RegisterPage implements OnInit {
 
   loginForm: FormGroup;
   user: any;
-  emailValue?: string;
+  emailValue: string = '';
   passValue?: string;
+  selectedRole: string = ''; // Inicializar selectedRole
   langs: string[] = [];
   idioma!: string;
 
   constructor(
     private router: Router,
-    private usuarios: UsuariosService,
     private formBuilder: FormBuilder,
     private transService: TranslateService,
     private AuthService: AuthService
   ) { 
     this.langs = this.transService.getLangs();
     this.loginForm = this.formBuilder.group({
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]]
+      email: [''],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      role: ['', Validators.required],
     });
   }
 
@@ -39,38 +39,47 @@ export class RegisterPage implements OnInit {
   }
   
   login() {
-    if(this.emailValue && this.passValue){
-      this.AuthService.login(this.emailValue,this.passValue);
- 
+    if (this.emailValue && this.passValue) {
+      this.AuthService.login(this.emailValue, this.passValue);
     }
-  
   }
   
   register() {
-    if(this.emailValue && this.passValue){
-      this.AuthService.register(this.emailValue,this.passValue);
- 
+    if (this.passValue && this.selectedRole) {
+      // Verifica si hay un correo electrónico, si no, genera uno basado en el rol
+      if (!this.emailValue) {
+        this.emailValue = this.generateEmailForRole(this.selectedRole);
+      }
+      
+      this.AuthService.register(this.emailValue, this.passValue);
     }
-  
   }
 
   carga() {
     Swal.fire({
       imageUrl: 'https://i.pinimg.com/originals/6b/e0/89/6be0890f52e31d35d840d4fe2e10385b.gif',
       imageHeight: 100,
-      showConfirmButton: false, // Oculta el botón "OK"
-      allowOutsideClick: false, // Evita que se cierre haciendo clic afuera
+      showConfirmButton: false,
+      allowOutsideClick: false,
       heightAuto: false
     });
 
-    // Simula un proceso de carga (puedes reemplazarlo con tu lógica real)
     setTimeout(() => {
       Swal.close();
-      // Redirige a la página 'home' después de cerrar el mensaje de carga
-    }, 1500); // Cambia el tiempo de espera según tus necesidades
+    }, 1500);
   }
 
-  changeLangs(event: any){
+  changeLangs(event: any) {
     this.transService.use(event.detail.value);
+  }
+
+  // Genera un correo electrónico basado en el rol seleccionado
+  generateEmailForRole(role: string): string {
+    if (role === 'pasajero') {
+      return 'pasajero@example.com'; // Puedes personalizar el dominio según tus necesidades
+    } else if (role === 'conductor') {
+      return 'conductor@example.com'; // Puedes personalizar el dominio según tus necesidades
+    }
+    return '';
   }
 }
--- src/app/services/firebase/auth.service.ts
+++ src/app/services/firebase/auth.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
 
-
 @Injectable({
   providedIn: 'root'
 })
@@ -12,21 +11,37 @@ export class AuthService {
 
   async login(email: string, pass: string){
     try {
-      const user = await this.auth.signInWithEmailAndPassword(email,pass);
-      this.router.navigate(['home']);
+      const user = await this.auth.signInWithEmailAndPassword(email, pass);
+      this.handleUserRedirect(user);
       console.log(user);
     } catch (error) {
-      console.error('Error en login: ',error);
+      console.error('Error en login: ', error);
     }
   }
 
   async register(email: string, pass: string){
     try {
-      const user = await this.auth.createUserWithEmailAndPassword(email,pass);
-      this.login(email,pass);
+      const user = await this.auth.createUserWithEmailAndPassword(email, pass);
+      await this.handleUserRedirect(user);
       console.log(user);
     } catch (error) {
-      console.error('Error en register: ',error);
+      console.error('Error en register: ', error);
+    }
+  }
+
+  async handleUserRedirect(user: any) {
+    if (user) {
+      const userEmail = user.user.email;
+      if (userEmail) {
+        if (userEmail.endsWith('@gmail.com')) {
+          this.router.navigate(['viajes-p']);
+        } else if (userEmail.endsWith('@conductor.com')) {
+          this.router.navigate(['viajes-c']);
+        } else {
+          console.error('Correo no compatible');
+          // Puedes manejar el caso en el que el correo no es compatible
+        }
+      }
     }
   }
 
@@ -34,7 +49,7 @@ export class AuthService {
     try {
       await this.auth.signOut(); 
     } catch (error) {
-     console.error('error en el logout',error);
+     console.error('error en el logout', error);
     }
   }
 
